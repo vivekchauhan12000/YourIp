9b16f30182ba91738cecf9792459bbd8be930b71
vivek <vivekchauhaniit@gmail.com>
2020-09-27 21:14:07 +0530

    Updates

---
+++ precache-manifest.ee10356576df31537048f44504d2c6a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83569abf6f4248569fa0772a1be651e",
+    "url": "/YourIp/index.html"
+  },
+  {
+    "revision": "8b1d06c532ec7bc3429b",
+    "url": "/YourIp/static/css/main.ddb12ffc.chunk.css"
+  },
+  {
+    "revision": "9468b780079e641ac9b8",
+    "url": "/YourIp/static/js/2.e0562b67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/YourIp/static/js/2.e0562b67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b1d06c532ec7bc3429b",
+    "url": "/YourIp/static/js/main.45662d36.chunk.js"
+  },
+  {
+    "revision": "c7b3f2119f5ede79a76e",
+    "url": "/YourIp/static/js/runtime-main.608bd9d4.js"
+  },
+  {
+    "revision": "dfc7fc946fa704c2b3f7131690a62124",
+    "url": "/YourIp/static/media/modernart.dfc7fc94.jpg"
+  }
+]);
