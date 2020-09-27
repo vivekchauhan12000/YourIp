f9c30f7f14678f2b824da7f76535927ccb61da5b
vivek <vivekchauhaniit@gmail.com>
2020-09-27 20:16:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react'
 import './App.css';
 import axios from 'axios';
+//import { Navbar } from 'react-bootstrap';
+import Navbar from './Navbar';
 
-export default class App extends Component {
-  
-  state={advice:''};
+class App extends React.Component {
+  state = {
+    advice: '',
+    ip:'',
+    zipcode:'',
+    lat:'',
+    log:'',
+  }
 
-  componentDidMount(){
+   componentDidMount() {
      this.fetchAdvice();
   }
-  
-  fetchAdvice=()=>{
-    axios.get('https://api.adviceslip.com/advice')
-    .then((response)=>{
-      const {advice} =  response.data.slip;
-      console.log(advice);
-      this.setState({advice})
-    })
-    .catch((error)=>{
+
+   fetchAdvice(){
+     axios.get('http://ip-api.com/json/')
+      .then((response) => {
+        const advice = response.data.country;
+        const ip = response.data.query;
+        const zipcode=response.data.zip;
+        const lat=response.data.lat;
+        const log=response.data.lon;
+        this.setState({ advice });
+        this.setState({ ip });
+        this.setState({ zipcode });
+        this.setState({ lat });
+        this.setState({ log });
+        //console.log(response.data.country);
+        console.log(advice);
+        console.log(ip);
+        console.log(zipcode);
+        console.log(lat);
+        console.log(log);
+
+
+      })
+      .catch((error) => {
         console.log(error);
-    });
+      });
   }
-  
-  
+
   render() {
-    const { advice }=this.state;
     return (
+       
+
       <div className="app">
-     <div className="card">
-        <h1 className="heading">
-         {advice}
-       </h1>
-       <button className="button" onClick={this.fetchAdvice}>
-         <span>
-          New one
-         </span>
-       </button>
-       </div>
+        
+        <Navbar/>
+       
+       
+        <div className="card">
+        <h2 className="cont">IP: {this.state.ip}</h2>
+          <h2 className="cont">Country: {this.state.advice}</h2>
+          <h2 className="cont">Zipcode: {this.state.zipcode}</h2>
+          <h2 className="cont">Latitude: {this.state.lat}</h2>
+          <h2 className="cont">Longitude: {this.state.log}</h2>
+          
         </div>
-     
+      </div>
     );
   }
 }
 
-
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+//import {Switch,Redirect} from 'react-router-dom'
+
+export default function Navbar() {
+  return(
+  <header className='navbar'>
+        <div className='navbar__title navbar__item'>YourIP</div>
+        <div className='navbar__item'>Developed by</div>
+        <div className='navbar__item'>Vivek Chauhan</div>
+           
+    </header>)
+}
